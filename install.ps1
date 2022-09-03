fcdf3e554735f8666bd87788951964ad1c74c299
Niki Akbar <djnixy@users.noreply.github.com>
2022-09-03 22:06:38 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,13 +1,13 @@
 # ESSENTIALS
-choco install firefox
+choco install `
+firefox `
+googlechrome `
+opera-gx `
+opera `
+microsoft-windows-terminal `
+git `
+docker-desktop `
+lens `
+notepadplusplus `
+wsl2 `
 
-# 
-choco install wsl2 notepadplusplus lens git docker-desktop -y
-choco install notepadplusplus --version 8.4.4 -y
-choco install lens --version 6.0.1 -y
-choco install git --version 2.37.3 -y
-choco install docker-desktop --version 4.11.1 -y
-choco install googlechrome --version 105.0.5195.54 -y
-choco install opera-gx --version 90.0.4480.78 -y
-choco install firefox --version 104.0.1 -y
-choco install microsoft-windows-terminal --version 1.14.2281.0 -y
